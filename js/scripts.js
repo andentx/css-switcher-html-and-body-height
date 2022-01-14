9e8c85ff85a7139360532c5323cee9999f5e5eef
Andrew D <56039221+andentx@users.noreply.github.com>
2022-01-14 11:19:45 -0500

    Set default to expanded

--- js/scripts.js
+++ js/scripts.js
@@ -3,32 +3,32 @@ const expandSectionButton = document.getElementById(`expand-button`);
 
 // Default is Expanded
 
-// function handleExpandedSection() {
-//     if (expandedSection.style.display === 'none') {
-//         expandedSection.style.display = 'flex';
-//         expandSectionButton.textContent = 'Minimize Container';
-//         return;
-//     }
-
-//     if (expandedSection.style.display !== 'none') {
-//         expandedSection.style.display = 'none';
-//         expandSectionButton.textContent = 'Click to Expand';
-//         return;
-//     }
-// }
-
-// Default is Minimized
-
 function handleExpandedSection() {
-    if (expandedSection.style.display === 'flex') {
-        expandedSection.style.display = 'none';
-        expandSectionButton.textContent = 'More Info';
-        return;
-    }
-
-    if (expandedSection.style.display !== 'flex') {
+    if (expandedSection.style.display === 'none') {
         expandedSection.style.display = 'flex';
         expandSectionButton.textContent = 'Minimize Container';
         return;
     }
+
+    if (expandedSection.style.display !== 'none') {
+        expandedSection.style.display = 'none';
+        expandSectionButton.textContent = 'Click to Expand';
+        return;
+    }
 }
+
+// Default is Minimized
+
+// function handleExpandedSection() {
+//     if (expandedSection.style.display === 'flex') {
+//         expandedSection.style.display = 'none';
+//         expandSectionButton.textContent = 'More Info';
+//         return;
+//     }
+
+//     if (expandedSection.style.display !== 'flex') {
+//         expandedSection.style.display = 'flex';
+//         expandSectionButton.textContent = 'Minimize Container';
+//         return;
+//     }
+// }
